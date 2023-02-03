2bc46bc7e88d17be4aa29e86dc8d1aad315bb860
Connie Leung <cleung334@gmail.com>
2023-02-04 00:28:18 +0800

    chore: redirect not found to index.html

--- vite.config.ts
+++ vite.config.ts
@@ -3,6 +3,8 @@ import { fileURLToPath, URL } from 'node:url'
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 
+console.log('process.env.NODE_ENV', process.env.NODE_ENV)
+
 // https://vitejs.dev/config/
 export default defineConfig({
   base: process.env.NODE_ENV === 'production' ? '/vue-dynamic-form-demo/' : '',
